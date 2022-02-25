fecd7c863b55720011a3a637f9bf49612aa97103
BasmaJawad <basma.jawad02@gmail.com>
2022-02-25 09:35:42 +0100

    added askAboutAge

--- src/Demo/Main.java
+++ src/Demo/Main.java
@@ -1,9 +1,22 @@
 package Demo;
 
+import javax.security.sasl.SaslClient;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("det virker");
         System.out.println("hej B");
+        new Main().askAboutAge();
+    }
+    int askAboutAge(){
+        Scanner in = new Scanner(System.in);
+        int age;
+
+        System.out.println("How old are you?");
+        age = in.nextInt();
+        return age;
+
     }
 }
